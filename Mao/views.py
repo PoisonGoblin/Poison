1ea1536c2b10cb7e2655dcaeeb41d649758d870f
PoisonGoblin <15882191694@163.com>
2019-12-13 12:20:49 +0800

    Django初步架构

--- Mao/views.py
+++ Mao/views.py
@@ -11,7 +11,7 @@ def index(request):
 
 def showdevice(request):
     from Mao import models
-    Info = models.Device.objects.all().filter(d_name='主机')
+    Info = models.Device.objects.all()
     return render(request, '../templates/show.html', {'Info': Info})
 
 
@@ -31,3 +31,15 @@ def add_device(request):
         Device_list = models.Device.objects.all()
         return render(request, '../templates/show.html', {'Info': Device_list})
     return render(request, '../templates/add.html')
+
+
+def search_device(request):
+    from Mao import models
+    if request.method == "POST":
+        name = request.POST.get("d_name", None)
+        type = request.POST.get("d_type", None)
+        date = request.POST.get("d_date", None)
+        user_class_id = request.POST.get("user_class_id", None)
+        search_Info = models.Device.objects.all().filter(d_name=name)
+        return render(request, '../templates/show.html', {'Info': search_Info})
+    return render(request, '../templates/search.html')
