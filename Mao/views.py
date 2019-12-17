3ead81ed30e3034ca28609881f6b39fee80be1b6
PoisonGoblin <15882191694@163.com>
2019-12-17 15:04:46 +0800

    Django初步架构

--- Mao/views.py
+++ Mao/views.py
@@ -12,14 +12,30 @@ def index(request):
 def showdevice(request):
     from Mao import models
     Info = models.Device.objects.all()
+    Info_model = models.Class.objects.all()
     if request.method == "POST":
         name = request.POST.get("d_name", None)
-        if name == '':
-            return render(request, '../templates/show.html', {'Info': Info})
+        type = request.GET.get("type", None)
+        # 搜索框、方案为空
+        if name == '' and type == '':
+            return render(request, '../templates/show.html', {'Info': Info, 'Info_model': Info_model})
+        # 搜索框为空，方案不为空
+        # if name == '' and type != '':
+        #     search_Info = models.Device.objects.all().filter(user_class_id=type)
+        #     return render(request, '../templates/show.html', {'Info': search_Info, 'Info_model': Info_model})
+        # 搜索框不为空，方案为空
+        if name != '' and type == '':
+            search_Info_name = models.Device.objects.all().filter(d_name=name)
+            return render(request, '../templates/show.html', {'Info': search_Info_name, 'Info_model': Info_model})
+        if type == '':
+            return HttpResponse(type)
+        # 搜索框、方案均不为空
         else:
-            search_Info = models.Device.objects.all().filter(d_name=name)
-            return render(request, '../templates/show.html', {'Info': search_Info})
-    return render(request, '../templates/show.html', {'Info': Info})
+            search_Info_name = models.Device.objects.all().filter(d_name=name)
+            search_Info_type = models.Device.objects.all().filter(d_type=name)
+            search_Info = search_Info_name | search_Info_type
+            return render(request, '../templates/show.html', {'Info': search_Info, 'Info_model': Info_model})
+    return render(request, '../templates/show.html', {'Info': Info, 'Info_model': Info_model})
 
 
 def add_device(request):
@@ -44,8 +60,13 @@ def add_device(request):
 
 def search_device(request):
     from Mao import models
+    search_Info_model = models.Class.objects.all()
     if request.method == "POST":
         name = request.POST.get("d_name", None)
         search_Info = models.Device.objects.all().filter(d_name=name)
         return render(request, '../templates/show.html', {'Info': search_Info})
-    return render(request, '../templates/search.html')
+    if request.method == "GET":
+        type = request.GET.get("type", None)
+        search_Info = models.Class.objects.all().filter(class_name=type)
+        return render(request, '../templates/show.html', {'Info_model': search_Info})
+    return render(request, '../templates/search.html', {'Info_model': search_Info_model})
